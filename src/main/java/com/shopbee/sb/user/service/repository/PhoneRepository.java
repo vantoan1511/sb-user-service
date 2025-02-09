d48b910ae018a75817a8e953c660b6bf8ff9e3e1
Toan Nguyen <vantoan1517@gmail.com>
2025-02-09 12:30:27 +0700

    Added phone repo

---
+++ src/main/java/com/shopbee/sb/user/service/repository/PhoneRepository.java
@@ -0,0 +1,22 @@
+/*
+ * PhoneRepository.java
+ *
+ * Copyright by sb-user-service, all rights reserved.
+ * MIT License: https://mit-license.org
+ */
+
+package com.shopbee.sb.user.service.repository;
+
+import com.shopbee.sb.user.service.model.Phone;
+import com.shopbee.sb.user.service.model.PhoneId;
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class PhoneRepository implements PanacheRepositoryBase<Phone, PhoneId> {
+
+    public boolean existedById(PhoneId phoneId) {
+        return findByIdOptional(phoneId).isPresent();
+    }
+
+}
